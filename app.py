3fd88ad65a2ffa99ce8056d76617984035b252b0
zoohta <koh.0430.ta@gmail.com>
2023-03-15 00:07:01 +0900

    add extracting code function

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 import streamlit as st
 import pandas as pd
+import re
 from langchain.agents import create_pandas_dataframe_agent
 from langchain.llms import OpenAI
+import sys
 
 uploaded_file = st.file_uploader("CSVファイルをアップロードしてください")
+
+
+class Logger():
+    stdout = sys.stdout
+    messages = []
+
+    def start(self):
+        sys.stdout = self
+
+    def stop(self):
+        sys.stdout = self.stdout
+
+    def write(self, text):
+        self.messages.append(text)
+
+
+log = Logger()
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
@@ -15,5 +34,15 @@ if uploaded_file is not None:
     with st.container():
         query = st.text_input('このデータについて知りたいことはなんですか？')
         if query:
+            log.start()
             ans = agent.run(query)
-            st.write(ans)
+            log.stop()
+            value = list(log.messages)
+            st.write(f'回答: {ans}')
+            st.write('')
+            if len(value) > 0:
+                match = re.search(r'Action Input: (.+?)\x1b', value[2])
+                if match:
+                    value = match.group(1)
+                    st.write(f'実行されたコード: {value}')
+
